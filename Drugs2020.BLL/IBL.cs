c50ec103c06d9e4e774a63008019a1f894bbfb87
ronip <ronipakter@gmail.com>
2020-10-11 00:42:45 +0300

    ג'קא

---
+++ Drugs2020.BLL/IBL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Drugs2020.BLL.BE;
+
+namespace Drugs2020.BLL
+{
+    public interface IBL
+    {
+        UserType IdentifyUser(string userId);
+        bool AutheticatePassward(string password);
+    }
+}
