f14367a847cc862e919e35cd013565b1a54e4f50
moshew1991 <moshew1991@gmail.com>
2020-10-14 17:26:12 +0300

    קצת הוספות שנמחקו

--- Drugs2020.PL/ViewModels/PatientDataViewModel.cs
+++ Drugs2020.PL/ViewModels/PatientDataViewModel.cs
@@ -13,22 +13,61 @@ namespace Drugs2020.PL.ViewModels
     {
         private PatientDataModel patientDataModel; 
         public event PropertyChangedEventHandler PropertyChanged;
-        public Patient Patient
-        {
-            get { return patientDataModel.Patient; }
-            set 
-            { 
-                patientDataModel.Patient = value;
-                if (PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("Patient"));
-                }
-            }
-        }
+        
+
         public PatientDataViewModel()
         {
             patientDataModel = new PatientDataModel();
         }
-       
+
+        public string ID
+        {
+            get { return patientDataModel.Patient.ID; }
+            set { patientDataModel.Patient.ID = value; }
+        }
+
+        public string FName
+        {
+            get { return patientDataModel.Patient.FirstName; }
+            set { patientDataModel.Patient.FirstName = value; }
+        }
+
+        public string LName
+        {
+            get { return patientDataModel.Patient.LastName; }
+            set { patientDataModel.Patient.LastName = value; }
+        }
+
+        public string Phone
+        {
+            get { return patientDataModel.Patient.Phone; }
+            set { patientDataModel.Patient.Phone = value; }
+        }
+
+        public string Email
+        {
+            get { return patientDataModel.Patient.Email; }
+            set { patientDataModel.Patient.Email = value; }
+        }
+
+        public string Address
+        {
+            get { return patientDataModel.Patient.Address; }
+            set { patientDataModel.Patient.Address = value; }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return patientDataModel.Patient.BirthDate; }
+            set { patientDataModel.Patient.BirthDate = value; }
+        }
+
+        public Sex SexOfPatient
+        {
+            get { return patientDataModel.Patient.Sex; }
+            set { patientDataModel.Patient.Sex = value; }
+        }
+
+
     }
 }
