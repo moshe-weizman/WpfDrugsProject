60b9da43df8d5cb2aed2d5fb93bc052079b813e1
ronip <ronipakter@gmail.com>
2020-10-25 23:18:39 +0200

    סדר וניקיון בBL

--- Drugs2020.BLL/IBL.cs
+++ Drugs2020.BLL/IBL.cs
@@ -8,24 +8,30 @@ namespace Drugs2020.BLL
 {
     public interface IBL
     {
-        bool ValidatePassword(IUser user, string password);
-        List<Drug> GetDrugsTakenPatient(string id);
-
-        List<Drug> GetDrugsPreviouslyTakenPatient(string id);
+        bool ValidatePassword(IUser user, string password);      
         IUser IdentifyUser(string userID);
 
-        void AddMedicalFileToPatient(MedicalFile medicalFile);
-        void UpdateMedicalFile(string patientId, MedicalFile medicalFile);
+        #region Recept
         void AddRecept(Recept recept);
         List<Recept> GetAllReceptsOfPatient(string id);
-        List<MedicalRecord> GetAllMedicalRecordsOfPatient(string patientId);
         List<Recept> GetAllReceptsByDate(DateTime startDate, DateTime endDate);
         List<Recept> GetAllReceptsByDrug(string drugIdCode);
         void CreatePDF(Recept recept);
+        #endregion
+
+        #region MedicalFile
+        void AddMedicalFileToPatient(MedicalFile medicalFile);
+        void UpdateMedicalFile(string patientId, MedicalFile medicalFile);
         MedicalFile GetMedicalFile(string patientID);
         bool MedicalFileAlreadyExists(MedicalFile medicalFile);
+        List<MedicalRecord> GetAllMedicalRecordsOfPatient(string patientId);
+        #endregion
+
+        #region MedicalRecord
         bool MedicalRecordAlreadyExists(MedicalRecord medicalRecord);
         void UpdateMedicalRecord(string medicalRecordID, MedicalRecord medicalRecord);
+        #endregion
+        
         #region Patient CRUD Functions
         void AddPatient(Patient patient);
         Patient GetPatient(string id);
@@ -42,15 +48,17 @@ namespace Drugs2020.BLL
         void DeletePhysician(string id);
         #endregion
 
-        #region Drug CRUD Functions
+        #region Drug
         void AddDrug(Drug drug);
         Drug GetDrug(string id);
         List<Drug> GetAllDrugs();
+        List<Drug> GetDrugsTakenPatient(string id);
+        List<Drug> GetDrugsPreviouslyTakenPatient(string id);
         void UpdateDrug(string id, Drug updatedDrug);
         void DeleteDrug(string id);
         #endregion
 
-        #region ActiveIngredient CRUD Functions
+        #region ActiveIngredient
         void AddActiveIngredient(ActiveIngredient ingredient);
         List<ActiveIngredient> GetActiveIngredientsOfDrug(string DrugIdCode);
         void UpdateActiveIngredient(ActiveIngredient ingredient);
