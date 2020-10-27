ca7a37bb59bf4dc8e465dab6187bfab60fd1b522
ipewzner <48483845+ipewzner@users.noreply.github.com>
2020-10-27 14:13:06 +0200

    seed data

---
+++ Drugs2020.DAL/SeedData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using Drugs2020.BLL.BE;
+
+namespace Drugs2020.DAL
+{
+    class SeedData : DropCreateDatabaseAlways<PharmacyContext>
+    {
+            protected override void Seed(PharmacyContext context)
+            {
+
+            #region Drug   
+            
+            IList<Drug> defaultSDrugs = new List<Drug>();
+            defaultSDrugs.Add(new Drug() {/*add drug here*/ });
+            defaultSDrugs.Add(new Drug() { });
+            context.Drugs.AddRange(defaultSDrugs);
+
+            #endregion
+
+            #region Patient   
+
+            IList<Patient> defaultSPatient = new List<Patient>();
+            defaultSPatient.Add(new Patient() { /*add Patient here*/ });
+            defaultSPatient.Add(new Patient() { });
+            context.Patients.AddRange(defaultSPatient);
+
+            #endregion
+            base.Seed(context);
+            }
+        }
+
+    }
+
